4800d3d04963eaedf55bb94d90be2a2981189a52
Santiago Orozco <splitnature@protonmail.com>
2021-11-02 23:45:12 +0000

    two

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  window.onload = function() {
+    let pronoun = ["the", "our"];
+    let adj = ["great", "big", "red"];
+    let noun = ["jogger", "racoon"];
+
+    const array = [];
+
+    for (let i = 0; i < pronoun.length; i += 1) {
+      for (let j = 0; j < adj.length; j += 1) {
+        for (let k = 0; k < noun.length; k += 1) {
+          const a = pronoun[i];
+          const b = adj[j];
+          const c = noun[k];
+          array.push(a + b + c);
+        }
+      }
+    }
+
+    console.log(array);
+  };
+
