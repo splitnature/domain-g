9893c521ea94a8d6604026ca3188b1120bf63ee1
Santiago Orozco <splitnature@protonmail.com>
2021-11-03 00:41:22 +0000

    2

--- src/app.js
+++ src/app.js
@@ -5,24 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-  window.onload = function() {
-    let pronoun = ["the", "our"];
-    let adj = ["great", "big", "red"];
-    let noun = ["jogger", "racoon"];
+window.onload = function() {
+  document.querySelector("#excuse").innerHTML = potato();
+};
 
-    const array = [];
+let pronoun = ["the", "our"];
+let adj = ["great", "big", "red"];
+let noun = ["jogger", "racoon"];
 
-    for (let i = 0; i < pronoun.length; i += 1) {
-      for (let j = 0; j < adj.length; j += 1) {
-        for (let k = 0; k < noun.length; k += 1) {
-          const a = pronoun[i];
-          const b = adj[j];
-          const c = noun[k];
-          array.push(a + b + c);
-        }
+const potato = () => {
+  const array = [];
+
+  for (let i = 0; i < pronoun.length; i += 1) {
+    for (let j = 0; j < adj.length; j += 1) {
+      for (let k = 0; k < noun.length; k += 1) {
+        const a = pronoun[i];
+        const b = adj[j];
+        const c = noun[k];
+        array.push(`<li>${a + b + c}</li>`);
       }
     }
-
-    console.log(array);
-  };
-
+  }
+  return array;
+};
